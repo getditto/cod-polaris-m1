73bcb0f5f0175f767176150c2f5d5d79d4c278f4
Aaron Fabbri <aaron.fabbri@ditto.live>
2024-01-05 21:13:28 -0800

    Add unit test for Config, using new Jest setup

---
+++ src/config.test.ts
@@ -0,0 +1,15 @@
+import { Config } from './config'
+
+test('config get', () => {
+    const config = new Config("config.json.example")
+
+    expect(config.getStr('APP_ID')).toBe('your-app-id-here')
+    expect(config.getBool('USE_BLE')).toBe(true)
+
+    // wrong type
+    expect(() => config.getBool('APP_ID')).toThrow()
+
+    // crazy key
+    expect(() => config.getStr('blah blah blah')).toThrow()
+
+})
