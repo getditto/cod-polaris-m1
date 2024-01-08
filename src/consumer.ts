75d0a010fe4532396be79a202b089fd76b87e233
Aaron Fabbri <aaron.fabbri@ditto.live>
2024-01-08 15:22:38 -0800

    Serve latest image from consumer webui when enabled.

--- src/consumer.ts
+++ src/consumer.ts
@@ -8,6 +8,10 @@ import {
 } from '@dittolive/ditto'
 import { DEFAULT_WEBUI_PORT } from './default'
 import Fastify, { FastifyInstance } from 'fastify'
+import { existsSync, mkdirSync } from 'fs'
+import fastifyStatic from '@fastify/static'
+import { pageWithImage } from './html'
+import path from 'path'
 
 export interface ConsumerStats {
     uniqueRecords: number
@@ -25,6 +29,8 @@ export class Consumer {
     imagesFetched: number
     webUi: boolean
     fastify: FastifyInstance | null
+    lastImage: string | null
+    imagePath: string
 
     constructor(
         ditto: Ditto,
@@ -42,6 +48,12 @@ export class Consumer {
         this.imagesFetched = 0
         this.webUi = webUi
         this.fastify = null
+        this.lastImage = null
+        this.imagePath = '/tmp/images'
+        // ensure dir exists
+        if (!existsSync(this.imagePath)) {
+            mkdirSync(this.imagePath)
+        }
     }
 
     async start(): Promise<void> {
@@ -76,31 +88,40 @@ export class Consumer {
                     if (attach == null) {
                         console.debug('--> fetch returned null')
                     } else {
-                        const outfile = `/tmp/fetched-${this.uniqueCount}.jpg`
+                        const outfile = `${this.imagePath}/fetched-${this.uniqueCount}.jpg`
                         console.debug(`--> writing ${outfile}..`)
                         await attach!.copyToPath(outfile)
+                        this.lastImage = outfile
                     }
                 }
             }
         })
     }
 
-    /* XXX
-    async handleWebGet(request, response ): Promise<void> {
-    }
-    */
-
     async startWebUI(): Promise<void> {
         console.info(`--> Starting web UI on port ${DEFAULT_WEBUI_PORT}`)
         this.fastify = Fastify({ logger: true })
+
+        this.fastify!.register(fastifyStatic, {
+            root: this.imagePath,
+            prefix: '/img/',
+        })
+
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        this.fastify.get('/', async (_req, rep) => {
-            rep.send({ hello: 'world' })
+        this.fastify!.get('/', async (_req, rep) => {
+            let webPath: string | null = null
+            if (this.lastImage != null) {
+                webPath = path.join('/img', path.basename(this.lastImage))
+            }
+            rep.type('text/html').send(pageWithImage(webPath))
         })
 
         const start = async () => {
             try {
-                await this.fastify!.listen({ port: DEFAULT_WEBUI_PORT })
+                await this.fastify!.listen({
+                    host: '0.0.0.0',
+                    port: DEFAULT_WEBUI_PORT,
+                })
             } catch (err) {
                 this.fastify!.log.error(err)
                 process.exit(1)
