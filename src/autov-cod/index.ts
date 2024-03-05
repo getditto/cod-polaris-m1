7207844c355f687c61d1cbbc49026becea3cca30
Aaron Fabbri <aaron.fabbri@ditto.live>
2024-03-04 18:52:30 -0800

    Add new run script for base-cod, allow passing config filename

--- src/autov-cod/index.ts
+++ src/autov-cod/index.ts
@@ -3,18 +3,24 @@ import { Config } from '../common-cod/config.js'
 import { HttpServer } from './http_server.js'
 
 function usage() {
-    console.log('Usage: node index.js')
-    console.log('  Arguments go in ./config.json')
+    console.log('Usage: node index.js [config-filename]')
+    console.log('  <config-filename> defaults to ./config.json')
 }
 
 async function main() {
-    if (process.argv.length != 2) {
-        usage()
-        return
+    let configFn = './config.json'
+    if (process.argv.length > 2) {
+        const a = process.argv[2]
+        if (process.argv.length != 3 || a == '-h' || a == '--help') {
+            usage()
+            return
+        } else {
+            configFn = a
+        }
     }
     let config
     try {
-        config = new Config('./config.json')
+        config = new Config(configFn)
     } catch (e) {
         usage()
         throw e
