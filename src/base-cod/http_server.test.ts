060fcbdadba998d3a80dd1afd16ed22536279f63
Aaron Fabbri <aaron.fabbri@ditto.live>
2024-03-06 17:51:26 -0800

    More base API server functionality & tests
    - Accept valid format Trial Start and End messages, returning 200 Created
      status.
    - Return human-readable error in response body, per interface spec.
    - Unit tests for the above items.

--- src/base-cod/http_server.test.ts
+++ src/base-cod/http_server.test.ts
@@ -2,18 +2,48 @@ import axios from 'axios'
 import { HttpServer } from './http_server.js'
 import { Config } from '../common-cod/config.js'
 import { DittoCOD } from '../ditto_cod.js'
-import { v0TrialWait } from '../common-cod/protocol.js'
+import {
+    Geometry,
+    Timestamp,
+    TrialId,
+    v0TrialEnd,
+    v0TrialStart,
+    v0TrialWait,
+} from '../common-cod/protocol.js'
 import { HttpStatus } from '../common-cod/http_base.js'
 
-test('base http server', async () => {
-    const config = new Config('./base-config.json.example')
-    const dittoCod = new DittoCOD(config.toDittoConfig())
-    const httpServer = new HttpServer(dittoCod, config)
-    await httpServer.start()
+class TestServer {
+    private httpServer: HttpServer | null = null
+    async start() {
+        const config = new Config('./base-config.json.example')
+        const dittoCod = new DittoCOD(config.toDittoConfig())
+        this.httpServer = new HttpServer(dittoCod, config)
+        await this.httpServer!.start()
+    }
+    async stop() {
+        await this.httpServer!.stop()
+    }
+
+    getPort(): number {
+        return this.httpServer!.base.config.port
+    }
+}
+
+const testServer = new TestServer()
+
+beforeAll(async () => {
+    await testServer.start()
+})
+
+afterAll(async () => {
+    await testServer.stop()
+})
+
+test('base health check', async () => {
     // For now: POST Wait message to /api/trial_start and confirm 200 response
     const initMsg = new v0TrialWait()
     const postData = initMsg.serialize()
-    const url = `http://localhost:${httpServer.base.config.port}/api/trial_start`
+    const url = `http://localhost:${testServer.getPort()}/api/trial_start`
     await axios
         .post(url, postData)
         .then((res) => {
@@ -35,8 +65,83 @@ test('base http server', async () => {
         .catch((err) => {
             expect(err.response.status).toBe(HttpStatus.BadRequest)
         })
+})
 
-    console.log('stopping http server')
-    await httpServer.stop()
-    console.log('finished http server init test')
+describe('base trial start', () => {
+    const ts = new Timestamp()
+    const id = TrialId.fromString('350.1.0')
+    const n = 12
+    const geom = Geometry.polygon([
+        [-50.12, 30.12],
+        [-51.12, 32.09],
+        [-49.91, 32.101],
+        [-50.12, 30.12],
+    ])
+    const startMsg = new v0TrialStart(ts, id, n, geom)
+    const postData = startMsg.serialize()
+    test('valid trial start', async () => {
+        const url = `http://localhost:${testServer.getPort()}/api/trial_start`
+        await axios
+            .post(url, postData)
+            .then((res) => {
+                expect(res.status).toBe(HttpStatus.Created)
+            })
+            .catch((err) => {
+                expect(err).toBeNull()
+            })
+    })
+
+    test('invalid start trial_id', async () => {
+        const url = `http://localhost:${testServer.getPort()}/api/trial_start`
+        // have to bypass strong typing to mess up id
+        const untypedObj = JSON.parse(postData)
+        untypedObj.trial_id = '12.3'
+        await axios
+            .post(url, untypedObj)
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            .then((_res) => {
+                // axios rejects promises on non-200 status codes
+                expect(false).toBeTruthy()
+            })
+            .catch((err) => {
+                expect(err.response.status).toBe(HttpStatus.UnprocessableEntity)
+                expect(err.response.data).toContain('Invalid trial_id')
+            })
+    })
+})
+
+describe('base trial end', () => {
+    const ts = new Timestamp()
+    const id = TrialId.fromString('350.1.0')
+    const startMsg = new v0TrialEnd(ts, id)
+    const postData = startMsg.serialize()
+    test('valid trial end', async () => {
+        const url = `http://localhost:${testServer.getPort()}/api/trial_end`
+        await axios
+            .post(url, postData)
+            .then((res) => {
+                expect(res.status).toBe(HttpStatus.Created)
+            })
+            .catch((err) => {
+                expect(err).toBeNull()
+            })
+    })
+
+    test('invalid end trial_id', async () => {
+        const url = `http://localhost:${testServer.getPort()}/api/trial_end`
+        // have to bypass strong typing to mess up id
+        const untypedObj = JSON.parse(postData)
+        untypedObj.trial_id = '12.3.213x'
+        await axios
+            .post(url, untypedObj)
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            .then((_res) => {
+                // axios rejects promises on non-200 status codes
+                expect(false).toBeTruthy()
+            })
+            .catch((err) => {
+                expect(err.response.status).toBe(HttpStatus.UnprocessableEntity)
+                expect(err.response.data).toContain('Invalid trial_id')
+            })
+    })
 })
