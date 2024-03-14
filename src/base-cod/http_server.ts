525a3b2c21e8b4ef19afd03a09458e085d462e1a
Aaron Fabbri <aaron.fabbri@ditto.live>
2024-03-14 15:02:51 -0700

    cod: More serde logic, error handling..
    Since the REST API depends on request body fields for routing (versus
    just URL suffixes), the server may have to JSON.parse() requests before
    knowning which type of object they are.
    
    The serde code needs to deal with this third case; "deserializing" from
    untyped js objects.

--- src/base-cod/http_server.ts
+++ src/base-cod/http_server.ts
@@ -1,6 +1,8 @@
 import { IncomingMessage, ServerResponse } from 'node:http'
 import { Config } from '../common-cod/config.js'
 import {
+    TrialEndObjV0,
+    TrialStartObjV0,
     v0TrialEnd,
     v0TrialStart,
     v0TrialWait,
@@ -31,25 +33,26 @@ export class HttpServer {
     }
 
     private async handleStart(
-        body: string,
+        obj: TrialStartObjV0,
         req: IncomingMessage,
         rep: ServerResponse
     ) {
+        console.debug('Received trial Start:', obj)
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const tStart = v0TrialStart.fromString(body)
+        const tStart = v0TrialStart.fromObject(obj)
         await this.trialModel.startTrial(tStart)
         rep.writeHead(HttpStatus.Created, CORS_ALLOW_ANY)
         rep.end()
     }
 
     private async handleEnd(
-        body: string,
+        obj: TrialEndObjV0,
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         _req: IncomingMessage,
         rep: ServerResponse
     ) {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const tEnd = v0TrialEnd.fromString(body)
+        const tEnd = v0TrialEnd.fromObject(obj)
         await this.trialModel.endTrial(tEnd)
         rep.writeHead(HttpStatus.Created, CORS_ALLOW_ANY)
         rep.end()
@@ -77,15 +80,16 @@ export class HttpServer {
                 rep.end(err)
             } else {
                 if (init.name == 'Trial Start') {
-                    await this.handleStart(body, req, rep)
+                    await this.handleStart(init, req, rep)
                 } else if (init.name == 'Trial End') {
-                    await this.handleEnd(body, req, rep)
+                    await this.handleEnd(init, req, rep)
                 } else if (init.name == 'Wait') {
                     // This is not in the spec--but would be good to have a
                     // side-effect free "ping" or "get status" endpoint to
                     // sanity/health check the service
                     await this.handleWait(body, req, rep)
                 } else {
+                    console.warn("Unsupported 'name' field:", init.name)
                     rep.writeHead(HttpStatus.BadRequest)
                     rep.end()
                 }
