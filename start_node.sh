306a649cf07e0204d01b601730b8399533e7a81e
Kit Plummer <kit@ditto.live>
2024-03-18 17:32:15 -0400

    set tag of the commit

--- start_node.sh
+++ start_node.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
-# trap ctrl-c and call ctrl_c()
-trap ctrl_c INT
-
-function ctrl_c() {
-    echo "** Trapped CTRL-C"
-}
 
 args_array=("$@")
 for i in "${args_array[@]}"; do
     :
     echo "### Got variable $i ###"
 done
-echo "args_count = $#"
+
 eval "$@"
